c3b552958e7e424d3876c7e576815d28daa61b26
tcfritchman <tcfritchman@gmail.com>
2022-12-01 21:29:01 -0800

    first commit

---
+++ src/main/kotlin/Day1.kt
@@ -0,0 +1,22 @@
+import kotlin.random.Random
+
+fun main() {
+
+    fun part1(input: String): Any {
+        return input.split("\n\n")
+            .maxOf { it -> it.split("\n").sumOf { it.toInt() } }
+    }
+
+    fun part2(input: String): Any {
+        return input.split("\n\n")
+            .map { it -> it.split("\n").sumOf { it.toInt() } }
+            .sorted()
+            .takeLast(3)
+            .sum()
+    }
+
+    val input = readInput("input1.txt")
+    printAnswer { part1(input) }
+    printAnswer { part2(input) }
+}
+
---
+++ src/main/kotlin/Utility.kt
@@ -0,0 +1,11 @@
+fun readInput(path: String): String {
+    val resource = {}::class.java.getResource(path)
+    return resource?.readText() ?: throw IllegalArgumentException("This input does not exist: $path")
+}
+
+fun printAnswer(f:() -> Any) {
+    val timerStart = System.currentTimeMillis()
+    val answer = f()
+    val timerStop = System.currentTimeMillis()
+    println("Answer: $answer - calculated in ${timerStop - timerStart}ms")
+}
