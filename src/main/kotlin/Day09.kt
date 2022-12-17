00fb0425c3fa89d4fce2d5d2039b61345879bb3f
tcfritchman <tcfritchman@gmail.com>
2022-12-17 13:24:13 -0800

    Day 9

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -2,19 +2,21 @@ import kotlin.math.abs
 
 fun main() {
 
+    fun runSimulation(input: String, size: Int) = input.split("\n")
+        .flatMap {
+            val symbol = it.split(" ")[0]
+            val times = it.split(" ")[1].toInt()
+            List(times) { MoveDirection.from(symbol) }
+        }
+        .fold(Simulation(size)) { simulation, direction -> simulation.take(direction) }
+        .countUniqueTailPositions()
+
     fun part1(input: String): Any {
-        return input.split("\n")
-            .flatMap {
-                val symbol = it.split(" ")[0]
-                val times = it.split(" ")[1].toInt()
-                List(times) {MoveDirection.from(symbol)}
-            }
-            .fold(Simulation())  { simulation, direction -> simulation.take(direction) }
-            .countUniqueTailPositions()
+        return runSimulation(input, size = 2)
     }
 
     fun part2(input: String): Any {
-        return input.split("\n").count()
+        return runSimulation(input, size = 10)
     }
 
     val input = readInput("input09.txt")
@@ -39,31 +41,35 @@ enum class MoveDirection(val dy: Int, val dx: Int) {
     }
 }
 
-class Simulation {
-    private var hy = 0
-    private var hx = 0
-    private var ty = 0
-    private var tx = 0
-    private val visitedTailPositions = mutableSetOf(Pair(0, 0))
+class Simulation(val size: Int) {
+    private val knots = List(size) { Knot(0, 0) }
+    private val head = knots[0]
+    private val tail = knots[size - 1]
+    private val visitedTailPositions = mutableSetOf(Knot(0, 0))
 
     fun take(direction: MoveDirection): Simulation {
         // Move head
-        hy += direction.dy
-        hx += direction.dx
+        head.y += direction.dy
+        head.x += direction.dx
 
-        // Move tail if needed
-        if (abs(hy - ty) > 1 || abs(hx - tx) > 1) {
-            ty += sign(hy - ty)
-            tx += sign(hx - tx)
+        // Move tail segments as needed
+        for (i in 1 until size) {
+            val current = knots[i]
+            val previous = knots[i - 1]
+            if (abs(previous.y - current.y) > 1 || abs(previous.x - current.x) > 1) {
+                current.y += sign(previous.y - current.y)
+                current.x += sign(previous.x - current.x)
+            }
         }
 
         // Update the set of visited tail positions
-        visitedTailPositions.add(Pair(tx, ty))
-
+        visitedTailPositions.add(tail.copy())
         return this
     }
 
-    fun countUniqueTailPositions():Int = visitedTailPositions.size
+    fun countUniqueTailPositions(): Int = visitedTailPositions.size
+
+    private data class Knot(var x: Int, var y: Int)
 }
 
 fun sign(x: Int): Int = if (x > 0) {
